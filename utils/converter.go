2bea8a466eb6a66b87ded33d4a883734deb06638
h4rimu <abdul.halim@digiasia.asia>
2020-07-28 16:42:21 +0700

    add function SetApiUrl

--- utils/converter.go
+++ utils/converter.go
@@ -3,6 +3,7 @@ package utils
 import (
 	"encoding/json"
 	"encoding/xml"
+	"net/url"
 )
 
 func JsonToString(uniqueID string, param interface{}) string {
@@ -25,4 +26,13 @@ func XmlToString(uniqueID string, param interface{}) string {
 
 	strResult := string(result)
 	return strResult
+}
+
+func SetApiUrl(uniqueID string, urlApi string) *url.URL {
+	url, err := url.Parse(urlApi)
+	if err != nil {
+		log.Errorf(uniqueID, "Error occurred %s ", err.Error())
+		return nil
+	}
+	return url
 }
