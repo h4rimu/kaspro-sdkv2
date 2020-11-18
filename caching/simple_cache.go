8ea46c2ec7f41d47d6b4886ffd2ea3172bf91859
h4rimu <abdul.halim@digiasia.asia>
2020-11-18 12:42:44 +0700

    add cache with hash

--- caching/simple_cache.go
+++ caching/simple_cache.go
@@ -17,6 +17,8 @@ type CacheFunction interface {
 	CreateNewCache() *cache.Cache
 	SetValue(key string, value string, cache cache.Cache)
 	GetValue(key string, cache cache.Cache) *interface{}
+	SetValueWithHash(key string, value string, cache cache.Cache)
+	GetValueWithHash(key string, cache cache.Cache) *interface{}
 }
 
 func (c *SimpleCache) CreateNewCache() *cache.Cache {
@@ -29,12 +31,29 @@ func (c *SimpleCache) CreateNewCache() *cache.Cache {
 
 func (c *SimpleCache) SetValue(key string, value string, globalCache cache.Cache) {
 
+	globalCache.Set(key, value, time.Minute*time.Duration(c.ExpiredTime))
+
+}
+
+func (c *SimpleCache) GetValue(key string, globalCache cache.Cache) *interface{} {
+
+	data, found := globalCache.Get(key)
+	if found {
+		return &data
+	}
+
+	return nil
+
+}
+
+func (c *SimpleCache) SetValueWithHash(key string, value string, globalCache cache.Cache) {
+
 	globalCache.Set(utils.EncryptionSha256(key), utils.EncryptionSha256(value),
 		time.Minute*time.Duration(c.ExpiredTime))
 
 }
 
-func (c *SimpleCache) GetValue(key string, globalCache cache.Cache) *interface{} {
+func (c *SimpleCache) GetValueWithHash(key string, globalCache cache.Cache) *interface{} {
 
 	data, found := globalCache.Get(utils.EncryptionSha256(key))
 	if found {
